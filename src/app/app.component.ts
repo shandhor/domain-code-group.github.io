df91b36dd14b6fd7907591a477370f67cb922fe0
shandhor <mohammedshan92@hotmail.com>
2026-08-11 23:21:16 +0300

    تعديلات شاملة على صفحات الموقع

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SeoService } from './services/SeoService';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {
   navigateToDcgERP() { 
